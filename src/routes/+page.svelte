16448ef5a66d2f246b77d0f9b9958be0353a46e3
Charles Pletcher <cpletcher2@gmail.com>
2026-03-24 17:24:03 -0400

    Change landing page link text to "Open the commentary"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,7 @@
 						<a
 							href={resolve('/passages/[urn]', { urn: "urn:cts:greekLit:tlg0525.tlg001:1.1" })}
 							class="rounded-md bg-yellow-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-yellow-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600 dark:bg-yellow-500 dark:hover:bg-yellow-400 dark:focus-visible:outline-yellow-500"
-							>Scroll 1 (Attica)</a
+							>Open the commentary</a
 						>
 					</div>
 				</div>
